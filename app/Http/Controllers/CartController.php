2905c9f838ed6d8378f937a46f01f51ab1fe8865
Marcus <marcuslxndr02@yahoo.com>
2026-02-15 18:59:22 -0400

    Ecommerce v3.5

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Traits\PhpFlasher;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+
+    //Add flasher here
+    use PhpFlasher;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
+
+        $user = Auth::user();
+
+        $cart_data = $user->products()->withPrices()->get();
+
+        $cart_data->calculateSubtotal();
+
+        return view('pages.default.cartpage', compact('cart_data'));
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Cart::updateOrCreate(
+            ['user_id' => Auth::id(), 'product_id' => $request->product_id],
+            ['quantity' => DB::raw('quantity +' . $request->quantity), 'updated_at' => now()]
+        );
+
+        $this->flashSuccess('Product Added To Cart');
+
+        return redirect()->route('cart.index')->with('message', 'Product added to cart');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function addToCartFromStore(Request $request)
+    {
+        Cart::updateOrCreate(
+            ['user_id' => Auth::id(), 'product_id' => $request->id],
+            ['quantity' => DB::raw('quantity +' . 1), 'updated_at' => now()]
+        );
+
+        $this->flashSuccess('Product Added To Cart');
+
+        return redirect()->route('cart.index')->with('message', 'Product added to cart');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        Cart::destroy($id);
+
+        $this->flashError('Product Removed from cart');
+
+
+        return redirect()->route('cart.index')->with('message', 'Product removed from cart');
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Traits\PhpFlasher;
+use Illuminate\Support\Facades\Auth;
+
+class ProductController extends Controller
+{
+
+    use PhpFlasher;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
+
+        $product_data = Product::withPrices()->paginate(6);
+
+        return view('pages.default.productspage', compact('product_data'));
+    }
+}
